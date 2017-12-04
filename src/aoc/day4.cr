72f918678c341349bc3230919668dbbb37acd17f
Arandi Lopez <arandilopez.93@gmail.com>
2017-12-03 23:50:45 -0600

    Day 4, part 2

--- src/aoc/day4.cr
+++ src/aoc/day4.cr
@@ -10,6 +10,16 @@ module Aoc
         carry
       }.compact.size
     end
+
+    def valid_passphrases2(matrix)
+      matrix
+      .map { |row| row.split }
+      .reduce([] of Array(String)) { |carry, row|
+        copy = row.map { |word| word.split("").sort.join("") }.uniq.compact
+        carry << copy if copy.size == row.size
+        carry
+      }.compact.size
+    end
   end
 end
 
@@ -17,4 +27,12 @@ puts Aoc::Day4.valid_passphrases([
   %{aa bb cc dd ee},
   %{aa bb cc dd aa},
   %{aa bb cc dd aaa}
+])
+
+puts Aoc::Day4.valid_passphrases2([
+  %{abcde fghij},
+  %{abcde xyz ecdab},
+  %{a ab abc abd abf abj},
+  %{iiii oiii ooii oooi oooo},
+  %{oiii ioii iioi iiio},
 ])
