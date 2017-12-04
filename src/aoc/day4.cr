7c115f4e98e73a854b03934261a97c4496f018b7
Arandi Lopez <arandilopez.93@gmail.com>
2017-12-03 23:55:39 -0600

    shorter** day 4 solutions

--- src/aoc/day4.cr
+++ src/aoc/day4.cr
@@ -2,21 +2,15 @@ module Aoc
   module Day4
     extend self
     def valid_passphrases(matrix)
-      matrix
-      .map { |row| row.split }
-      .select { |row|
-        copy = row.uniq.compact
-        copy == row
-      }.size
+      matrix.map { |row| row.split }
+        .select { |row| row == row.uniq.compact}.size
     end
 
     def valid_passphrases2(matrix)
-      matrix
-      .map { |row| row.split }
-      .select { |row|
-        copy = row.map { |word| word.split("").sort.join("") }.uniq.compact
-        copy.size == row.size
-      }.size
+      matrix.map { |row| row.split }
+        .select { |row|
+          row.size == row.map { |word| word.split("").sort.join("") }.uniq.compact.size
+        }.size
     end
   end
 end
