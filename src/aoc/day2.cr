5d726442e8c0074bfd9a71cc70904fa5ef69d880
Arandi Lopez <arandilopez.93@gmail.com>
2017-12-03 18:30:30 -0600

    day 2 part 2

--- src/aoc/day2.cr
+++ src/aoc/day2.cr
@@ -4,6 +4,17 @@ module Aoc
     def checksum(spreadsheet)
       spreadsheet.reduce(0) { |carry, row| carry + (row.max - row.min) }
     end
+
+    def checksum2(spreadsheet)
+      spreadsheet.map { |row|
+        result = 0
+        row.each_permutation(2) { |nums|
+          nums.sort!
+          result = (nums[1] / nums[0]) if (nums[1].modulo(nums[0]) == 0)
+        }
+        result
+      }.sum
+    end
   end
 end
 
@@ -12,5 +23,11 @@ test_spreadsheet = [
  [7, 5, 3],
  [2, 4, 6, 8]
 ]
-
 puts Aoc::Day2.checksum(test_spreadsheet)
+
+test_spreadsheet2 = [
+  [5, 9, 2, 8],
+  [9, 4, 7, 3],
+  [3, 8, 6, 5],
+]
+puts Aoc::Day2.checksum2(test_spreadsheet2)
