c4fd4354142c367a726c51141b5a1d68d1beb224
Arandi Lopez <arandilopez.93@gmail.com>
2017-12-06 00:26:42 -0600

    Day 6, part 2

--- spec/day6_spec.cr
+++ spec/day6_spec.cr
@@ -6,4 +6,8 @@ describe "Day 6" do
     Aoc::Day6.reallocate([0, 2, 7, 0]).should eq(5)
   end
 
+  describe "part 1" do
+    Aoc::Day6.reallocate_2([0, 2, 7, 0]).should eq(4)
+  end
+
 end
--- src/aoc/day6.cr
+++ src/aoc/day6.cr
@@ -19,5 +19,24 @@ module Aoc
       end
       return cycles
     end
+
+    def reallocate_2(banks)
+      cycles = 0
+      seen = [] of Array(Int32)
+      until seen.includes? banks
+        seen.push banks.dup
+        max = banks.max
+        index = banks.index(max) || -9999
+        return 0 if index == -9999
+        banks[index] = 0
+        while max != 0
+          index = (index + 1) % banks.size
+          banks[index] = (banks[index] + 1)
+          max -= 1
+        end
+        cycles += 1
+      end
+      return cycles - (seen.index(banks) || 0)
+    end
   end
 end
